3cda71ea88a16a6b836c70c7fe10fa46ddcad8af
haseeb <lalamax3d@gmail.com>
2022-12-22 23:32:26 +0500

    lazy loading dependency

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import './App.css';
 import Gallery from './Gallery';
+import JsonDataDisplay from './GeekTable';
 
 
 function App() {
   return (
     // <React.Fragment>
     <>
+      <JsonDataDisplay/>
       <h1 style={{textAlign:'center'}}> Image Gallery</h1>
       <h2 style={{textAlign:'center'}}> Please Subscribe</h2>
       <Gallery/>
